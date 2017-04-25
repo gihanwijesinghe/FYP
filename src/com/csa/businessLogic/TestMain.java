1f14488f5d56deba0d6aa22adf495c200d39c281
pranavan15 <Pranavan15@yahoo.com>
2017-04-25 13:40:39 +0530

    Adding Innings based  analysis

--- src/com/csa/businessLogic/TestMain.java
+++ src/com/csa/businessLogic/TestMain.java
@@ -3,7 +3,7 @@ package com.csa.businessLogic;
 /**
  * Created by Pranavan on 24-04-2017.
  */
-public class Main2 {
+public class TestMain {
     public static void main(String[] args) {
         System.out.println("6.3".split("\\.")[0]);
     }
---
+++ src/com/csa/entity/Dataset.java
@@ -0,0 +1,87 @@
+package com.csa.entity;
+
+/**
+ * Created by Gihan on 4/20/2017.
+ */
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Dataset {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int dataId;
+
+    private int totalRuns;
+    private double runRate;
+    private int totalWickets;
+    private String result;
+    private int runsPowerPlay;
+    private int wicketsPowerPlay;
+
+    public Dataset() {
+        // TODO Auto-generated constructor stub
+    }
+
+
+    public int getDataId() {
+        return dataId;
+    }
+
+    public void setDataId(int dataId) {
+        this.dataId = dataId;
+    }
+
+    public int getTotalRuns() {
+        return totalRuns;
+    }
+
+    public void setTotalRuns(int totalRuns) {
+        this.totalRuns = totalRuns;
+    }
+
+    public double getRunRate() {
+        return runRate;
+    }
+
+    public void setRunRate(double runRate) {
+        this.runRate = runRate;
+    }
+
+    public int getTotalWickets() {
+        return totalWickets;
+    }
+
+    public void setTotalWickets(int totalWickets) {
+        this.totalWickets = totalWickets;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public int getRunsPowerPlay() {
+        return runsPowerPlay;
+    }
+
+    public void setRunsPowerPlay(int runsPowerPlay) {
+        this.runsPowerPlay = runsPowerPlay;
+    }
+
+    public int getWicketsPowerPlay() {
+        return wicketsPowerPlay;
+    }
+
+    public void setWicketsPowerPlay(int wicketsPowerPlay) {
+        this.wicketsPowerPlay = wicketsPowerPlay;
+    }
+}
+
