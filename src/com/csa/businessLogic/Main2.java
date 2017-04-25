bbd58782028781174be3c784018a4470094a3848
pranavan15 <Pranavan15@yahoo.com>
2017-04-25 13:23:01 +0530

    Adding Innings based  analysis

---
+++ src/com/csa/businessLogic/Main2.java
@@ -0,0 +1,10 @@
+package com.csa.businessLogic;
+
+/**
+ * Created by Pranavan on 24-04-2017.
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("6.3".split("\\.")[0]);
+    }
+}
